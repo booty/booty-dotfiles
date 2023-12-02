661b721846161a3cbd63b2609c08bb66c95675e5
John Rose <johnedmundrose@gmail.com>
2023-12-01 22:27:52 -0500

    convenience func

--- functions/massmatrix.zsh
+++ functions/massmatrix.zsh
@@ -9,4 +9,5 @@ alias mm.check="ruff . --fix && mypy --strict-optional --follow-imports=skip api
 alias mm.c="mm.check"
 alias mm.test="mm.check && pytest"
 alias mm.t="mm.test"
+alias mz="cd ~/code/mzml_web && va"
 # alias mm.pgwatch="watch -n 2 psql -h localhost -q -p 5423 -d mm_dev -U postgres -c \"select id, name, extension, hash, state from datafiles;\""
