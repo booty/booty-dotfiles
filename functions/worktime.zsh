c6bcaf562ef01b0d69aefd513762d0ee97c44687
John Rose <johnedmundrose@gmail.com>
2024-01-09 08:32:24 -0500

    Add sites to block; don't launch webbrick; only kill FF if changes made

--- functions/worktime.zsh
+++ functions/worktime.zsh
@@ -1,28 +1,29 @@
 # if the contents of src are equal to the contents of /etc/hosts, print a message that says "ok". if the contents are inequal, replace /etc/hosts with src and print a message that says "updated hosts file"
 function update_hosts_and_relaunch_firefox() {
   if [ "$(cat $1)" = "$(cat /etc/hosts)" ]; then
-    echo "hosts file seems ok already!"
+    echo "Hosts file seems ok already!"
+    sudo killall -HUP mDNSResponder
   else
     sudo cp -f "$1" /etc/hosts
     sudo killall -HUP mDNSResponder
-    echo "updated hosts file"
+    echo "Updated hosts file. Killing and relaunching Firefox!"
+    # Firefox will cache DNS lookups
+    pkill Firefox
+    open -a Firefox
   fi
-  # Only flush DNS cache if necessary
-  pkill Firefox
-  open -a Firefox
 }
 
 # copy ./worktime/hosts-worktime to /etc/hosts, forcing an overwrite
 function worktime() {
   echo "Getting ready to work...?"
   update_hosts_and_relaunch_firefox "$HOME/booty-dotfiles/functions/worktime/hosts-worktime"
-  nohup ruby ~/booty-dotfiles/functions/worktime/worktime-webrick.rb >/dev/null 2>&1 &
-  echo "Worktime hosts file installed - kick some ass."
+  # nohup ruby ~/booty-dotfiles/functions/worktime/worktime-webrick.rb >/dev/null 2>&1 &
+  echo "Kick some ass! 💪"
 }
 
 function funtime() {
   echo "Work is over! Or... is it?"
-  `pkill -f worktime-webrick.rb`
+  # `pkill -f worktime-webrick.rb`
   update_hosts_and_relaunch_firefox "$HOME/booty-dotfiles/functions/worktime/hosts-funtime"
   echo "OK, relax and browse the web."
 }
