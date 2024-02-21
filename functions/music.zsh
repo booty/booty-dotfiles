16363237e8e444b2d27a7533b3f371b1c758dba6
John Rose <johnedmundrose@gmail.com>
2024-02-21 09:21:49 -0500

    Add recursive version of alac cmd

--- functions/music.zsh
+++ functions/music.zsh
@@ -2,7 +2,10 @@
 # ░░░░░░░░░█░█░█░█░▀▀█░░█░░█░░░░░░░░░░░░
 # ░░░░░░░░░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░░░░░░░░░░
 
-mp3all() { parallel -i -j6 ffmpeg -i {} -qscale:a -vcodec copy  0 {}.mp3 ::: ls ./*.flac }
-aacall() { parallel -i -j6 ffmpeg -i {} -vn -c:a libfdk_aac -afterburner 1 -vbr 5 -vcodec copy  {}.m4a ::: ls ./*.flac; }
-aacall2() { parallel -i -j6 ffmpeg -i {} -vn -c:a libfdk_aac -vbr 5 -vcodec copy  {}.m4a ::: ls ./*.m4a; }
-alacall() { parallel -i -j6 ffmpeg -i {} -acodec alac -vcodec copy {}.m4a ::: ls ./*.flac; }
+music.mp3all() { parallel -i -j{$(nproc)} ffmpeg -i {} -qscale:a -vcodec copy  0 {}.mp3 ::: ls ./*.flac }
+music.aacall() { parallel -i -j{$(nproc)} ffmpeg -i {} -vn -c:a libfdk_aac -afterburner 1 -vbr 5 -vcodec copy  {}.m4a ::: ls ./*.flac; }
+music.aacall2() { parallel -i -j{$(nproc)} ffmpeg -i {} -vn -c:a libfdk_aac -vbr 5 -vcodec copy  {}.m4a ::: ls ./*.m4a; }
+music.alacall() { parallel -i -j{$(nproc)} ffmpeg -i {} -acodec alac -vcodec copy {}.m4a ::: ls ./*.flac; }
+music.alacall_recurse() {
+    find . -type f -name '*.flac' | parallel -i -j{$(nproc)} ffmpeg -i {} -acodec alac -vcodec copy {}.m4a
+}
