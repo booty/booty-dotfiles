d3f80aaaecaf92cdb0a64ebfe7412faab9d1c8ba
John Rose <johnedmundrose@gmail.com>
2025-05-28 21:35:26 -0400

    Python aliases

--- functions/python.zsh
+++ functions/python.zsh
@@ -10,9 +10,12 @@ alias da="deactivate"
 # iPython
 alias ip="ipython"
 alias ipp="ipython -i"
+alias ipy="ipython -i -c 'import sys; sys.path.append(\".\")'"
+
+alias py="python"
+alias pym="python -m"
 
 # Good for running a single pytest
-alias py="python"
 alias pyt='ic && ENV=test pytest -vv'
 
 alias py.top_level_deps="pip list --not-required --format=freeze"
